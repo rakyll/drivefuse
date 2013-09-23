5f453d93dc0e11c04a6a9c5582d3b07e178c7878
Ali Afshar <afshar@google.com>
2013-09-23 15:04:40 -0700

    Added copyright notice.

--- cmd/authwizard.go
+++ cmd/authwizard.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package cmd contains the command-line user interface.
 package cmd
 
 import (
